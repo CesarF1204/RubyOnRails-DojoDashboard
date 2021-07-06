8b6ffc6494a55191ed831b863503695f7b00d442
Cesar Francisco <princexcesar@gmail.com>
2021-07-06 09:19:41 +0800

    added create feature

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -2,4 +2,11 @@ class DojosController < ApplicationController
     def index
         @dojos = Dojo.all
     end
+    def create
+        render 'create'
+    end
+    def create_process
+        Dojo.create(branch: params[:branch], street: params[:street], city: params[:city], state: params[:state])
+        redirect_to('/')
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "dojos#index"
+  get 'dojos/new' => 'dojos#create'
+  post 'dojos/create_process' => 'dojos#create_process'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
