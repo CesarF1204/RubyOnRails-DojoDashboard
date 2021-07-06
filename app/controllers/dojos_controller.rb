f56bb5e658a46bdeaef0b9fb5af72418e37ce657
Cesar Francisco <princexcesar@gmail.com>
2021-07-06 10:22:28 +0800

    added show, edit, and delete feature

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -9,4 +9,21 @@ class DojosController < ApplicationController
         Dojo.create(branch: params[:branch], street: params[:street], city: params[:city], state: params[:state])
         redirect_to('/')
     end
+    def show
+        @dojo = Dojo.find(params[:id])
+    end
+    def edit
+        @dojo = Dojo.find(params[:id])
+        render 'edit'
+    end
+    def update
+        dojo = Dojo.find(params[:id])
+        dojo.update(branch: params[:branch], street: params[:street], city: params[:city], state: params[:state])
+        redirect_to('/')
+    end
+    def destroy
+        dojo = Dojo.find(params[:id])
+        dojo.destroy
+        redirect_to('/')
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root "dojos#index"
   get 'dojos/new' => 'dojos#create'
   post 'dojos/create_process' => 'dojos#create_process'
+  get 'dojos/:id' => 'dojos#show'
+  get 'dojos/:id/edit' => 'dojos#edit'
+  patch 'edit_process/:id' => 'dojos#update'
+  delete 'dojos/:id' => 'dojos#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
